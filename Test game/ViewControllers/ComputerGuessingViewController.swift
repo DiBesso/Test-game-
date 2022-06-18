3676dae250c4e87caa1738ed4dd024ca36b16791
Dmitry <radiator211@mail.ru>
2022-06-19 01:28:36 +0700

    Work with logical in ComputerGuessingVC

--- Test game/ViewControllers/ComputerGuessingViewController.swift
+++ Test game/ViewControllers/ComputerGuessingViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ComputerGuessingViewController: UIViewController {
-
+    
     
     @IBOutlet weak var tryNumberLabel: UILabel!
     @IBOutlet weak var computerGuessingLabel: UILabel!
@@ -18,14 +18,66 @@ class ComputerGuessingViewController: UIViewController {
     @IBOutlet weak var equalButton: UIButton!
     @IBOutlet weak var moreButton: UIButton!
     
-    
+    private let namesForLabels = NamesForLabels ()
+    private let namesForButtons = NamesForButtons ()
+    private var tryNumber = 1
+    private var number = 100
+    private var randomNumber = Int.random(in: 1...100)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        setLabels()
+        setButtons()
     }
     
-
-
+    func setLabels() {
+        tryNumberLabel.text = namesForLabels.tryNumber + String(tryNumber)
+        computerGuessingLabel.text = namesForLabels.computerGuessing
+        computerVariantLabel.text = namesForLabels.computerVariant + String(randomNumber) + "?"
+        moreLessLabel.text = namesForLabels.moreLess
+    }
+    
+    
+    func setButtons() {
+        lessButton.setTitle(namesForButtons.less, for: .normal)
+        lessButton.setTitleColor(.black, for: .normal)
+        lessButton.backgroundColor = .white
+        lessButton.layer.cornerRadius = 10
+        lessButton.layer.borderWidth = 1
+        lessButton.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        equalButton.setTitle(namesForButtons.equal, for: .normal)
+        equalButton.setTitleColor(.black, for: .normal)
+        equalButton.backgroundColor = .white
+        equalButton.layer.cornerRadius = 10
+        equalButton.layer.borderWidth = 1
+        equalButton.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        moreButton.setTitle(namesForButtons.more, for: .normal)
+        moreButton.setTitleColor(.black, for: .normal)
+        moreButton.backgroundColor = .white
+        moreButton.layer.cornerRadius = 10
+        moreButton.layer.borderWidth = 1
+        moreButton.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+    
+    @IBAction func lessButtonAction(_ sender: Any) {
+        number = randomNumber
+        randomNumber = Int.random(in: 1...number)
+        tryNumber += 1
+        setLabels()
+    }
+    @IBAction func equalButtonAction(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func moreButtonAction(_ sender: Any) {
+        number = randomNumber
+        randomNumber = Int.random(in: number...100)
+        tryNumber += 1
+        setLabels()
+    }
+    
 }
