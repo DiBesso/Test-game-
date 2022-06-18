80044993cc35ff424d0d37911a04025cc3fc4932
Dmitry <radiator211@mail.ru>
2022-06-18 16:13:37 +0700

    Work with StartVC

--- Test game/Model/Model.swift
+++ Test game/Model/Model.swift
@@ -8,8 +8,28 @@
 import UIKit
 
 
-struct namesForLabels {
+struct NamesForLabels {
     let headerGame = "My Awesome Game"
     let tryNumber = "Try № "
+    let computerGuessing = "Computer is guessing"
+    let moreLess = "My number is "
+    let playerGuessing = "You are guessing"
+    let computerAnswerLess = "No, my number is less than yours"
+    let computerAnswerMore = "No, my number is more than yours"
+    let scores = "Scores:"
+    let playerTries = "Your's tries count: "
+    let computerTries = "Computer's tries count: "
+    let playerWin = "You win"
+    let computerWin = "Computer Win"
+}
+
+struct NamesForButtons {
+    let start = "Start New Game"
+    let enter = "Enter the Number"
+    let less = "<"
+    let equal = "="
+    let more = ">"
+    let guess = "Guess"
+    let mainMenu = "Main Menu"
 }
 
--- Test game/ViewControllers/StartViewController.swift
+++ Test game/ViewControllers/StartViewController.swift
@@ -12,14 +12,31 @@ class StartViewController: UIViewController {
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var startGameButton: UIButton!
     
+    private let namesForLabels = NamesForLabels ()
+    private let namesForButtons = NamesForButtons ()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setLabels()
+        setButtons()
+    }
+    
+    
+    
+
+    
+    func setLabels() {
+        headerLabel.text = namesForLabels.headerGame
     }
 
-
+    func setButtons() {
+        startGameButton.titleLabel?.text = namesForButtons.start
+        startGameButton.backgroundColor = .blue
+        startGameButton.layer.cornerRadius = 10
+        startGameButton.tintColor = .white
+    }
 }
 
